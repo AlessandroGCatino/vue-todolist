87a944fb215bb396ac82f0dd947641bb17d87f78
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-29 16:33:03 +0100

    create functions per aggiungere e rimuovere task

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ createApp({
             tasks: [
                 { 
                     text: "Preparare il pranzo",
-                    done: false 
+                    done: true 
                 },
                 { 
                     text: "Lavare il pavimento", 
@@ -32,6 +32,20 @@ createApp({
 
     },
     methods: {
+        deleteTask(index) {
+            this.tasks.splice(index, 1)
+        },
+        addTask() {
+            if (this.tasks.filter(e => e.text === this.newTask.text).length > 0) {
+                alert("Task già inserita")
+            } else if (this.newTask.text == ""){
+                alert("Non hai inserito Task")
+            } else {
+                let addNewTask = Object.assign({}, this.newTask);
+                this.tasks.push(addNewTask)
+                this.newTask.text = ""
+            }
+        }
 
     }
 
